38c79adea8eccd0c959c16e244b77ae65eb1d05c
wendi <ifyouseewendy@gmail.com>
2014-09-16 23:22:18 +0800

    Build 0.0.2, add routes

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,23 @@
 require "rulers/version"
 require 'rulers/array'
+require 'rulers/routes'
 
 module Rulers
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'}, ['Hello from Ruby on Rulers!']]
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(action)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+    def env
+      @env
     end
   end
 end
