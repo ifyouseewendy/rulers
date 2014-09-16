73b9dab52d4a7342927ff9a7ef5147e07bdbc3b4
wendi <ifyouseewendy@gmail.com>
2014-09-16 23:36:32 +0800

    Add exception rescue for call

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -9,10 +9,15 @@ module Rulers
         return [404, {'Content-Type' => 'text/html'}, []]
       end
 
-      klass, action = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(action)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      begin
+        klass, action = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(action)
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      rescue => e
+        puts "Exception raised: #{e.message}"
+        [500, {'Content-Type' => 'text/html'}, ['Server error']]
+      end
     end
   end
 
