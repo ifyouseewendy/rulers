75168fdc86606235445fdf0e8fc3f4dd572a3732
wendi <ifyouseewendy@gmail.com>
2014-09-21 14:17:33 +0800

    Call render automatically

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -29,12 +29,11 @@ module Rulers
         klass, action = get_controller_and_action(env)
         controller = klass.new(env)
         text = controller.send(action)
-        if controller.get_response
-          st, hd, rs = controller.get_response.to_a
-          [st, hd, [rs.body].flatten]
-        else
-          [200, {'Content-Type' => 'text/html'}, [text]]
-        end
+
+        # call render automatically
+        controller.render_response action.to_sym unless controller.get_response
+        st, hd, rs = controller.get_response.to_a
+        [st, hd, [rs.body].flatten]
       rescue => e
         puts "Exception raised: #{e.message}"
         [500, {'Content-Type' => 'text/html'}, ['Server error']]
